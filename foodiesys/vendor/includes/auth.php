64abcfdb117092767214110c056a3da24e5b0779
Harizikandar <134240537+Harizikandar@users.noreply.github.com>
2026-06-29 16:13:11 +0800

    Delete foodiesys directory

--- foodiesys/vendor/includes/auth.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/session.php';
-
-if (empty($_SESSION['vendor_id'])) {
-    header('Location: login.php');
-    exit;
-}
--- foodiesys/vendor/includes/session.php
+++
@@ -1,5 +0,0 @@
-<?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_name('foodiesys_vendor');
-    session_start();
-}
